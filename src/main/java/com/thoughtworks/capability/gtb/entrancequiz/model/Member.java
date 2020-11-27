a0c5af88549930adfa109db274dd56b46342c134
Huijuan Yang <huijuan.yang@Huijuans-MacBook-Pro.local>
2020-11-27 23:53:17 +0800

    last adjustment for entrance quiz in October

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/model/Member.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/model/Member.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.gtb.entrancequiz.model;
 
 public class Member {
+    private final Integer id;
     private final String name;
 
-    public Member(String name) {
+    public Member(Integer id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MemberService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MemberService.java
@@ -10,6 +10,24 @@ import java.util.List;
 public class MemberService {
     private List<Member> memberList = new ArrayList<>();
 
+    public MemberService() {
+        this.memberList.add(new Member(1, "成吉思汗"));
+        this.memberList.add(new Member(2, "鲁班七号"));
+        this.memberList.add(new Member(3, "太乙真人"));
+        this.memberList.add(new Member(4, "钟无艳"));
+        this.memberList.add(new Member(5, "花木兰"));
+        this.memberList.add(new Member(6, "雅典娜"));
+        this.memberList.add(new Member(7, "芈月"));
+        this.memberList.add(new Member(8, "白起"));
+        this.memberList.add(new Member(9, "刘禅"));
+        this.memberList.add(new Member(10, "庄周"));
+        this.memberList.add(new Member(11, "马超"));
+        this.memberList.add(new Member(12, "刘备"));
+        this.memberList.add(new Member(13, "哪吒"));
+        this.memberList.add(new Member(14, "大乔"));
+        this.memberList.add(new Member(15, "蔡文姬"));
+    }
+
     public List<Member> getAllMembers() {
         return memberList;
     }
